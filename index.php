82c78435c223fa50f99d4dd8fc7ad3acf7571798
hafiznanda292@gmail.com <whirryle@example.com>
2025-07-24 18:06:38 +0800

    menambahkan list di file index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
   <h1>HALO INI GAJAH PHP</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptatem repudiandae cum ad, voluptates veritatis. Rem esse ab debitis dolor!</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
